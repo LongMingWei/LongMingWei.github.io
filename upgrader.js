c41e3b70fe242831d50f980aa9c4315f13a079d5
LongMingWei <longmw2001@gmail.com>
2023-06-18 02:08:14 +0800

    Updated Home div

--- upgrader.js
+++ upgrader.js
@@ -1,13 +1,27 @@
+var upgrades = 0;
+
 $(".upgrade").click(function() {
-    $("body").children().each(function() {
-        $(this).addClass("upgraded");
-      });
+  upgrades++;
+  if (upgrades == 1) {
+    $("div#Home1").toggle();
+    $("div#Home").toggle();
+  }
 });
 
 $(document).ready(function() {
-  $('a').click(function(event) {
-    event.preventDefault(); 
-    const section = $($(this).attr('href'));
-    $('html, body').animate({ scrollTop: section.offset().top }, 'slow');
+  $('a.internal-link').click(function(event) {
+    const linkTarget = $(this).attr('href');
+    
+    if (linkTarget.startsWith('#')) {
+      event.preventDefault();
+      const section = $(linkTarget);
+      $('html, body').animate({ scrollTop: section.offset().top }, 'slow');
+    }
   });
+});
+
+var typed = new Typed('.typed', {
+  strings: ["web", "software", "game"],
+  typeSpeed: 50,
+  loop: true
 });
